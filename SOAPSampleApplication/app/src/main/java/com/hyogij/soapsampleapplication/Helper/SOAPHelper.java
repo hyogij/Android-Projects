d6663bc152b2021bdd47226baa06fc5f882dffe9
hyogij <hyogij@gmail.com>
2016-02-01 17:04:22 +0900

    Add a sample application to practice SOAP request

---
+++ SOAPSampleApplication/app/src/main/java/com/hyogij/soapsampleapplication/Helper/SOAPHelper.java
@@ -0,0 +1,81 @@
+package com.hyogij.soapsampleapplication.Helper;
+
+import android.util.Log;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.PropertyInfo;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+/**
+ * A class to request SOAP and to receive data
+ */
+public class SOAPHelper {
+    private static final String CLASS_NAME = SOAPHelper.class.getCanonicalName();
+
+    private static final String SOAP_ACTION = "http://www.w3schools" +
+            ".com/webservices/CelsiusToFahrenheit";
+    private static final String METHOD_NAME = "CelsiusToFahrenheit";
+    private static final String NAMESPACE = "http://www.w3schools.com/webservices/";
+    private static final String URL = "http://www.w3schools.com/webservices/tempconvert.asmx";
+
+    private static SoapPrimitive soapPrimitive = null;
+
+    public static void soapRequest(String celsius) {
+        try {
+            SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+            Request.addProperty("Celsius", celsius);
+
+            SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            soapEnvelope.dotNet = true;
+            soapEnvelope.setOutputSoapObject(Request);
+
+            HttpTransportSE transport = new HttpTransportSE(URL);
+
+            transport.call(SOAP_ACTION, soapEnvelope);
+            soapPrimitive = (SoapPrimitive) soapEnvelope.getResponse();
+
+            Log.d(CLASS_NAME, "Result Celsius: " + soapPrimitive);
+        } catch (Exception ex) {
+            Log.d(CLASS_NAME, "Error: " + ex.getMessage());
+        }
+    }
+
+    public static SoapPrimitive getSoapPrimitive() {
+        return soapPrimitive;
+    }
+
+    public static void getFahrenheit(String celsius) {
+        //Create request
+        SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+        //Property which holds input parameters
+        PropertyInfo celsiusPI = new PropertyInfo();
+        //Set Name
+        celsiusPI.setName("Celsius");
+        //Set Value
+        celsiusPI.setValue(celsius);
+        //Set dataType
+        celsiusPI.setType(double.class);
+        //Add the property to request object
+        request.addProperty(celsiusPI);
+        //Create envelope
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(
+                SoapEnvelope.VER11);
+        envelope.dotNet = true;
+        //Set output SOAP object
+        envelope.setOutputSoapObject(request);
+        //Create HTTP call object
+        HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+
+        try {
+            //Invole web service
+            androidHttpTransport.call(SOAP_ACTION, envelope);
+            //Get the response
+            soapPrimitive = (SoapPrimitive) envelope.getResponse();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SOAPSampleApplication/app/src/main/java/com/hyogij/soapsampleapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.hyogij.soapsampleapplication;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hyogij.soapsampleapplication.Helper.SOAPHelper;
+
+import org.ksoap2.serialization.SoapPrimitive;
+
+/**
+ * An activity calculating celsius to fahrenheit.
+ * References : https://trinitytuts.com/load-data-from-soap-web-service-in-android-application/,
+ * http://code.tutsplus.com/tutorials/consuming-web-services-with-ksoap--mobile-21242
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String CLASS_NAME = MainActivity.class.getCanonicalName();
+
+    private EditText etCelsius = null;
+    private String celsius = null;
+    private SoapPrimitive soapPrimitive = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etCelsius = (EditText) findViewById(R.id.etCelsius);
+    }
+
+    public void onCalculate(View view) {
+        celsius = etCelsius.getText().toString();
+        AsyncCallWS task = new AsyncCallWS();
+        task.execute();
+    }
+
+    private class AsyncCallWS extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            Log.d(CLASS_NAME, "onPreExecute");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d(CLASS_NAME, "doInBackground");
+//            SOAPHelper.soapRequest(celsius);
+            SOAPHelper.getFahrenheit(celsius);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Log.d(CLASS_NAME, "onPostExecute");
+            soapPrimitive = SOAPHelper.getSoapPrimitive();
+            Toast.makeText(MainActivity.this, "Response : " + soapPrimitive.toString(), Toast
+                    .LENGTH_SHORT).show();
+        }
+    }
+}
