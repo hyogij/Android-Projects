a7a6c0ec95f5ea0248ab26850e3eefa7be868360
hyogij <hyogij@gmail.com>
2016-02-04 11:03:57 +0900

    Modifies package name from upper case to lower case to followg coding convension

--- MasterDetailView/app/src/main/java/com/hyogij/masterdetailview/ItemListActivity.java
+++ MasterDetailView/app/src/main/java/com/hyogij/masterdetailview/ItemListActivity.java
@@ -4,17 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 import com.hyogij.masterdetailview.dummy.DummyContent;
 
